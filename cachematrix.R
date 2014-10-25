9fbbab9a9b1692fceca6e2e5fa8782b1fa89f663
Iskar Waluyo <iskarwaluyo@gmail.com>
2014-10-25 14:35:56 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function (makeCacheMatrix) creates a matrix with preset values and returns the inverse of the matrix.
+## The second function calculates the inverse of a matrix, but if "NULL" is entered as a value, the function
+## returns the inverse calculated in the first function.
 
-## Write a short comment describing this function
+## makeCacheMatrix - creates a 3x3 matrix of x, y and z vectors. The vector is cached as the variable "m"
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x,y,z){
+  x <- c(6,20,5) 
+  y <- c(1,2,7)
+  z <- c(6,8,12)
+  xy.matrix <- cbind(x,y,z)
+  xy.matrixinverse <- solve(xy.matrix)
+  m <<- xy.matrixinverse
+  return(m)
+  
 }
 
+## cacheSolve - returns the inverse of an inputed matrix, if no "NULL" is entered as the matrix, it returns 
+## the "m" matrix cached from the makeCacheMatrix function
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(n.matrix = matrix()){
+  
+    if(is.null(n.matrix)){
+        print("No Data - Returning Cache")
+        return(m)
+}   else{
+        solve(n.matrix)
+}
 }
